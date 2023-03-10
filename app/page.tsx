b50e43db9b76de3948577b791cca01639601c9b0
satoshi02198 <awareness_yoga@yahoo.co.jp>
2023-03-10 22:28:38 +0900

    202303102230

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="h-screen">
         <div className="w-[80%] max-w-lg flex flex-col justify-center items-center mx-auto mt-6 p-2 space-y-2 border-2 border-lime-700">
           <h1 className="text-md font-medium">
-            Let`s find your favarite quotes
+            Let`s find your favorite quotes
           </h1>
           <p className="text-sm">1, Click one of them</p>
           <p className="text-sm">2, Find quotes and collect!</p>
